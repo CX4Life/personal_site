7994c5e6ff2fcc51c8e521f3864c9ab22febbaee
tim woods <tim.woods.tw@gmail.com>
2019-11-03 21:32:42 -0800

    Add resume redirect

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-import { Home, Blog, ServiceWorkerDemo } from './Pages'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, Blog, Resume, ServiceWorkerDemo } from './Pages'
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 import Constants from './lib/Constants';
 import './App.css';
 
+const pdf_blob_url = 'https://sitestorage2019.blob.core.windows.net/images/tim_woods_resume.pdf?sp=rl&st=2019-11-04T03:03:12Z&se=2100-01-01T20:00:00Z&sv=2019-02-02&sr=b&sig=u6cwRvxyBaGY7voEYVTwz4T%2BTih6OnP9IpLlnIacBiM%3D'
 export default class App extends Component {
   constructor(props) {
     super(props)
@@ -34,6 +35,10 @@ export default class App extends Component {
         <div className="App">
           <Route exact path='/' component={Home} />
           <Route path='/blog' component={() => <Blog api={api} />} />
+          <Route path='/resume' component={() => {
+            window.location.href = pdf_blob_url;
+            return null;
+          }} />
           <Route path='/serviceWorker' component={() => <ServiceWorkerDemo api={api} />} />
         </div>
       </Router>
